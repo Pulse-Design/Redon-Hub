eaddfaa30e5f3769be90799e73cfd8d72730c6c0
Pulse-Design <166261678+Pulse-Design@users.noreply.github.com>
2024-04-10 21:50:47 -0700

    Update test.py

--- bot/cogs/test.py
+++ bot/cogs/test.py
@@ -2,31 +2,24 @@
     File: /bot/cogs/
     Usage: 
 """
-from discord.ext.commands import Cog, command
-from discord.app_commands import Choice, Choices
-from random import randint
-from discord import Interaction
+from discord.ext.commands import Cog
+from discord import app_commands, Interaction
 import logging
 
 _log = logging.getLogger(__name__)
 
 
-class Template(Cog):
+class Embed(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @app_commands.command()
+    async def hola(self, interaction: Interaction):
+        await interaction.response.send_message("hola")
     @Cog.listener()
     async def on_ready(self):
         _log.info(f"Cog {__name__} ready")
 
-    @command(name="random", description="Get a random number")
-    async def random_command(self, interaction: Interaction, min_: int = 0, max_: int = 100):
-        """
-        Generate a random number between min_ and max_.
-        """
-
-        await interaction.response.send_message(f"Your number is: {randint(min_, max_)}")
-
 
 async def setup(bot):
-    await bot.add_cog(Template(bot))
+    await bot.add_cog(Embed(bot))
