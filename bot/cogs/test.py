a515e632810aa8368151c5df8bbe435c987a6f15
Pulse-Design <166261678+Pulse-Design@users.noreply.github.com>
2024-04-10 22:02:38 -0700

    Update test.py

--- bot/cogs/test.py
+++ bot/cogs/test.py
@@ -1,15 +1,25 @@
-from discord.ext.commands import Cog, command
-from discord import Interaction, Embed
-import logging
-
-_log = logging.getLogger(__name__)
+from discord.ext.commands import Bot, Cog, Context, command
+from discord_slash import SlashContext, cog_ext
+from discord_slash.utils.manage_commands import create_option
+from discord import Embed
 
 class PredefinedEmbed(Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
-    @command()
-    async def embed(self, interaction: Interaction):
+    @cog_ext.cog_slash(
+        name="embed",
+        description="Send a predefined embed message",
+        options=[
+            create_option(
+                name="channel",
+                description="Channel to send the embed",
+                option_type=7,  # Type 7 represents a channel option
+                required=False
+            )
+        ]
+    )
+    async def send_embed(self, ctx: SlashContext, channel=None):
         # Create a predefined embed
         embed = Embed(
             title="Predefined Embed",
@@ -21,12 +31,14 @@ class PredefinedEmbed(Cog):
         embed.add_field(name="Field 2", value="This is field 2", inline=False)
         embed.set_footer(text="Sent by Bot")
 
-        # Send the predefined embed
-        await interaction.response.send_message(embed=embed)
+        # Determine where to send the embed
+        if channel:
+            target_channel = self.bot.get_channel(channel)
+        else:
+            target_channel = ctx.channel
 
-    @Cog.listener()
-    async def on_ready(self):
-        _log.info(f"Cog {__name__} ready")
+        # Send the predefined embed to the target channel
+        await target_channel.send(embed=embed)
 
-async def setup(bot):
+async def setup(bot: Bot):
     await bot.add_cog(PredefinedEmbed(bot))
